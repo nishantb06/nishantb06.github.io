d11d9efc9644f34465fc79e71b7708a6e4ead55b
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-08-28 09:20:54 +0530

    feat: rendering blogs from list dynamically

--- src/components/Writings.js
+++ src/components/Writings.js
@@ -1,23 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
 const Writings = () => {
     const [activeTab, setActiveTab] = useState('Blogs');
     const [searchTerm, setSearchTerm] = useState('');
+    const [blogPosts, setBlogPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchBlogPosts = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/blogs');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch blog posts');
+                }
+                const data = await response.json();
+                setBlogPosts(data);
+            } catch (error) {
+                console.error('Error fetching blog posts:', error);
+            }
+        };
+
+        fetchBlogPosts();
+    }, []);
 
     const tags = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen', 'Twenty'];
-
-    const blogPosts = [
-        {
-            title: 'Pushing New Rows to BigQuery Table in GCP using Go',
-            excerpt: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam sequi ipsum soluta accusamus asperiores, sunt velit, eos minus animi, cumque aperiam incidunt? Accusamus beatae itaque dolores numquam magnam nostrum autem.',
-            date: '12th June, 2024',
-            tags: ['One', 'Two', 'Three', 'Four'],
-            slug: 'bigquery-rows-go',
-            id: 1,
-        },
-        // Add more blog posts here...
-    ];
 
     return (
         <section className="section">
@@ -63,7 +69,7 @@ const Writings = () => {
                     {blogPosts.map((post, index) => (
                         <div key={index} className="px-6 py-5">
                             <Link to={`/writings/blogs/${post.slug}`} className="title is-3 has-text-weight-medium">{post.title}</Link>
-                            <h2 className="subtitle is-6">{post.excerpt}</h2>
+                            <h2 className="subtitle is-6">{post.subtitle}</h2>
                             <span className="is-size-6 pr-5">{post.date}</span>
                             {post.tags.map((tag, tagIndex) => (
                                 <Link to={`/writings/tags/${tag}`} key={tagIndex} className="tag is-normal is-link is-light mx-1">{tag}</Link>
