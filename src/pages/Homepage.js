32f6686a5e3294bf5fb97ceef3fac7049837ab73
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-09-05 09:11:54 +0530

    feat: cleaning up the homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import meImage from '../assets/IMG_0840.jpg';
+
 const HomePage = () => {
+    const socialLinks = [
+        { name: 'Twitter', icon: 'https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg', url: 'https://x.com/itsnishant14' },
+        { name: 'LinkedIn', icon: 'https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg', url: 'https://www.linkedin.com/in/nishantbhansali/' },
+        { name: 'GitHub', icon: 'https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/github.svg', url: 'https://github.com/nishantb06' },
+        { name: 'Kaggle', icon: 'https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/kaggle.svg', url: 'https://www.kaggle.com/nishantbhansali' },
+        { name: 'Gmail', icon: 'https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/gmail.svg', url: 'mailto:nbhansali06@gmail.com' },
+    ];
+
     return (
         <>
             <section className="section">
                 <div className="container">
-                    <h2 className="title">
-                        Hi, I'm Nishant, a Machine learning engineer
-                    </h2>
-                </div>
-            </section>
-
-            <section className="section">
-                <div className="container">
                     <div className="columns">
-                        <div className="column is-6">
+                        <div className="column is-9">
                             <div className="card">
                                 <div className="card-content">
-                                    <h2 className="title">Who am I?</h2>
+                                    <h1 className="title is-3">Hi, I'm Nishant, a Machine learning engineer</h1>
                                     <p>
-                                        Hi, my name is Nishant Bhansali. I'm working as a Machine Learning Engineer at Sharechat, working remotely from
+                                        I'm working as a Machine Learning Engineer at Sharechat, working remotely from
                                         Ahmedabad, India. I have majorly worked on solving Computer Vision and Digital Image Processing based problems, and
                                         that's where I would say my expertise lies. Be it recent transformer architectures or archaic Image processing
                                         algorithms, I have my hands dirtied by almost everything vision based. My work at Sharechat has been around Image
@@ -35,17 +36,6 @@ const HomePage = () => {
                                     <p>
                                         I am a huge sports buff. Played Basketball for roughly 10 years and currently exploring surfing and Kick-boxing!
                                     </p>
-                                    <p>
-                                        Contact me<br />
-                                        Email - nishantbhansali80@gmail.com<br />
-                                        Phone number - 9558428844
-                                    </p>
-
-                                    <h2 className="title mt-6">News</h2>
-                                    <ul>
-                                        <li>Bronze medal in Kaggle HubMap+HPA Medical Image Segmentation Competition (80/1175 participants). <Link to="#">Link</Link></li>
-                                        <li>Survey Paper on Vision Transformers for Edge Devices</li>
-                                    </ul>
 
                                     <h2 className="title mt-6">Why this blog?</h2>
                                     <p>
@@ -55,37 +45,29 @@ const HomePage = () => {
                                         as well.
                                     </p>
                                     <p>Thanks for spending your time on my blog!!</p>
+
+                                    <h2 className="title mt-6">News</h2>
+                                    <ul>
+                                        <li>Bronze medal in Kaggle HubMap+HPA Medical Image Segmentation Competition (80/1175 participants). <Link to="#">Link</Link></li>
+                                        <li>Survey Paper on Vision Transformers for Edge Devices</li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
-                        <div className="column is-6">
+                        <div className="column is-3">
                             <div className="card">
                                 <div className="card-image">
                                     <figure className="image">
                                         <img src={meImage} alt="Nishant Bhansali" />
                                     </figure>
                                 </div>
-                                <div className="card-content">
-                                    <div className="media">
-                                        <div className="media-left">
-                                            <figure className="image is-48x48">
-                                                <img src={meImage} alt="Nishant Bhansali" />
-                                            </figure>
-                                        </div>
-                                        <div className="media-content">
-                                            <p className="title is-4">Nishant Bhansali</p>
-                                            <p className="subtitle is-6">@itsnishant14</p>
-                                        </div>
-                                    </div>
-                    
-                                    <div className="content">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-                                        iaculis mauris. <a href="https://bulma.io">@bulmaio</a>. <a href="#">#css</a>
-                                        <a href="#">#responsive</a>
-                                        <br />
-                                        <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                                    </div>
-                                </div>
+                                <footer className="card-footer">
+                                    {socialLinks.map((link, index) => (
+                                        <a href={link.url} className="card-footer-item" key={index} aria-label={link.name} target="_blank" rel="noopener noreferrer">
+                                            <img src={link.icon} alt={link.name} width="24" height="24" />
+                                        </a>
+                                    ))}
+                                </footer>
                             </div>
                         </div>
                     </div>
