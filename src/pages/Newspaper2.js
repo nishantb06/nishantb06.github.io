2713da012aed54cf7b709a6dc52aa33e5ba13ab0
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-10-08 18:28:40 +0530

    newspaper 2

---
+++ src/pages/Newspaper2.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Newspaper2 = () => {
+  const backgroundStyle = {
+    backgroundImage: 'url("/blogs/texture.jpg")',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    minHeight: '100vh',
+    width: '100%',
+  };
+
+  return (
+    <div style={backgroundStyle}>
+      {/* Your content goes here */}
+    </div>
+  );
+};
+
+export default Newspaper2;
